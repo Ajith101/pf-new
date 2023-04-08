d6410e148bcae92b2557beb388d34a700b8ea438
Ajith101 <ajithpktd@gmail.com>
2023-04-08 15:20:53 +0530

    deleted some files

--- app.js
+++
@@ -1,8 +0,0 @@
-let menuToogle = document.querySelector(".menu-btn");
-let navMEnu = document.querySelector(".nav_menu");
-
-menuToogle.addEventListener("click", () => {
-  navMEnu.classList.toggle("active");
-  menuToogle.classList.toggle("open");
-  // menuToogle.classList.toggle("open");
-});
